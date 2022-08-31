22ac87adb86899164f5f157da599e0a04a809630
FrankVazquezSE <mhp3rdynot52@gmail.com>
2022-08-31 15:21:52 -0400

    Full React App with Stripe

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+
+const footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 JSM Headphones All Rights Reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default footer
